ef9458aa8bc79f5d878248aba13af8b8c8b24e3a
ViktoriaKomlik <vica93v@gmail.com>
2018-12-21 20:54:06 -0800

    1st commit

---
+++ test/vikatest.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+
+describe('Page opening', function () {
+
+    it('get title', function(){
+        browser.url('/'); //open baseUrl + string passed in .url() function
+        let title = browser.getTitle(); //get page title and assign it to the "title" variable
+        browser.pause(5000); //just pause to visually see that something is happening on the page
+        console.log(title); //log "title" variable
+        assert.equal(title, 'Bug Tracker', 'Title is incorrect'); //compare that "title" variable equals to "Bug Tracker" and error-message if not
+    })
+
+    it('get header title', function(){
+         let title = browser.getText('.custom-header span'); //get page title and assign it to the "title" variable
+         assert.equal(title, 'BugTracker', 'Title is incorrect'); //compare that "title" variable equals to "Bug Tracker" and error-message if not
+    })
+
+    it('click registration button', function(){
+        browser.click('#registration'); //get page title and assign it to the "title" variable
+        let result = browser.waitForVisible('button=< Back', 5000);
+        assert.equal(result, true, 'Button is not clicked'); //compare that "title" variable equals to "Bug Tracker" and error-message if not
+    })
+
+});
